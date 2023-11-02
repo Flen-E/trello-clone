327d4ca351a186458167083f1ef5294191a8aff2
Flen-E <cjj642@naver.com>
2023-11-02 22:42:19 +0900

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { useRecoilState } from "recoil";
-import { hourSelector, minuteState } from "./atoms";
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value); // +가 들어가면 숫자로 바꿔줌 숫자에 + "" 넣어서 문자열로 바꾸는거와 비슷한 느낌
-  };
-  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) =>{
-    setHours(+event.currentTarget.value);
-  };
-
-
+  const onDragEnd = () =>{};
   return ( 
-    <div>
-      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes"/>
-      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours"/>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => 
+          <ul>
+            <Draggable draggableId="first" index={0}>
+              {() => <li>One</li>}
+            </Draggable>
+            <Draggable draggableId="second" index={1}>
+              {() => <li>Two</li>}
+            </Draggable>
+            
+          </ul>
+          }
+
+
+        </Droppable>
+      </div>
+
+    </DragDropContext>
   );
 }
 
