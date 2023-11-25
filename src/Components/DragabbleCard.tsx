95261f098fe8245e0ad2618adab3e75a9a68ecb5
Flen-E <cjj642@naver.com>
2023-11-25 22:41:33 +0900

    update css

--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -7,7 +7,7 @@ const Card = styled.div<{isDragging: boolean}>`
   padding: 10px 10px;
   background-color: ${(props) => props.isDragging ? "#74b9ff" : props.theme.cardColor};
   margin-bottom: 5px;
-  box-shadow : ${(props) => props.isDragging ? "0px 2px 5px rgba(0,0,0,0.5)": "none"}
+  box-shadow : ${(props) => props.isDragging ? "0px 2px 5px rgba(0,0,0,0.6)": "none"}
 `;
 
 interface IDragabbleCardProps {
