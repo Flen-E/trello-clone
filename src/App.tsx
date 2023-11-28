fd3dcb4798cd99c31e09d2d3bf3d90b7b14fd88a
Flen-E <cjj642@naver.com>
2023-11-28 14:58:18 +0900

    update trello

--- src/App.tsx
+++ src/App.tsx
@@ -33,10 +33,9 @@ function App() {
       // same board movement.
       setToDos((allBoards) =>{
         const boardCopy = [...allBoards[source.droppableId]];
-        // 1) Delete item on source.index
+        const taskObj = boardCopy[source.index];
         boardCopy.splice(source.index, 1);
-        // 2) Put back the item on the destination.index
-        boardCopy.splice(destination?.index, 0, draggableId);
+        boardCopy.splice(destination?.index, 0, taskObj);
         return {
           ...allBoards,
           [source.droppableId] : boardCopy,
@@ -47,11 +46,12 @@ function App() {
       // cross board movement.
       setToDos((allBoards) =>{
         const sourceBoard = [...allBoards[source.droppableId]];
+        const taskObj = sourceBoard[source.index];
         const destinationBoard = [...allBoards[destination.droppableId]];
         // 1) Delete item on source.index
         sourceBoard.splice(source.index, 1);
         // 2) Put back the item on the destination.index
-        destinationBoard.splice(destination?.index, 0, draggableId);
+        destinationBoard.splice(destination?.index, 0, taskObj);
         return {
           ...allBoards,
           [source.droppableId] : sourceBoard,
